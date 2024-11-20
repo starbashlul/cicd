2213ec31e23b3b6a5cb4e9546ee45a0860f657f0
starbashlul <artem562356@yandex.ru>
2024-11-20 17:51:18 +0300

    fix

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+
+    @Test
+    public void shouldReturnNull() {
+        Assertions.assertEquals(1, 1);
+    }
+
+}
