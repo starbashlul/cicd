8c9db7f24546678259ad22a57b3306b432cc11d1
starbashlul <artem562356@yandex.ru>
2024-11-20 19:33:11 +0300

    release init

---
+++ src/test/java/MyNewTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MyNewTest {
+    @Test
+    public void shouldReturnNull() {
+        Assertions.assertEquals(1, 1);
+    }
+}
