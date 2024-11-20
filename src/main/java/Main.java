eb69679f4253e8bda20674a7f26def9ec3b4ee48
starbashlul <artem562356@yandex.ru>
2024-11-20 20:05:17 +0300

    release fix 9999999999999999999999999

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,4 +2,7 @@ public class Main {
     public static void main(String args[]) {
         System.out.println("LOL");
     }
+    public static void doSmth(String smth) {
+        System.out.println("do smth");
+    }
 }
